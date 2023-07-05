57ea68278ddf88dd73360da6dc26f8bcf5f479fa
Kane Clover <kane.clover@gmail.com>
2023-07-05 01:06:27 +0100

    chained call for contacts list

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
-import { useToast } from 'primevue/usetoast';
+import { useGetV2Community } from '@/api/v2/generated/community/community';
+import { usePostV2ContactFilter } from '@/api/v2/generated/contact/contact';
+import { watch } from 'vue';
 
-const toast = useToast();
+const community = useGetV2Community();
+// TODO: Why is this endpoint a POST?
+const contactFilter = usePostV2ContactFilter();
 
-const handleClick = () => {
-  toast.add({ severity: 'error', summary: 'lorem ipsum', detail: 'dfdsdfgdsgsdfg', life: 3000 });
-};
+watch(community.data, (data) => {
+  const communityId = data?.response?.communities[0].id;
+  console.log('watch', communityId);
+  if (communityId) {
+    contactFilter.mutate({
+      data: {
+        communityId,
+        filter: { searchTerm: '' },
+        page: 1,
+        contactsPerPage: 50
+      }
+    });
+  }
+});
 </script>
 
 <template>
   <h1 class="text-lg">Contacts</h1>
-  <Button label="Click me" @click="handleClick()" />
+  <pre> {{ JSON.stringify(contactFilter.data, null, 2) }}</pre>
 </template>
