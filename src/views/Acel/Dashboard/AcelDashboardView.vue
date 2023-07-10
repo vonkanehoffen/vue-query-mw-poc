e0ca39eed7a02b47df42e645aa3ce938a2d1a75f
Kane Clover <kane.clover@gmail.com>
2023-07-10 14:02:15 +0100

    using dropdown

--- src/views/Acel/Dashboard/AcelDashboardView.vue
+++ src/views/Acel/Dashboard/AcelDashboardView.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
 import { useGetV2AcelReportId } from '@/api/v2/generated/survey/survey';
 import Dropdown from 'primevue/dropdown';
-import Button from 'primevue/button';
-import { computed, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 
-const selectedReportId = ref('');
-// const reports = ref([
-//   { name: 'Babergh', code: 'pt' },
-//   { name: 'Willis', code: 'zm' },
-//   { name: 'Another one', code: 'kioy' }
-// ]);
-// watch(selectedReportId, (newValue) => {
-//   console.log(newValue);
-// });
-
-// const code = computed(() => selectedReportId.value && selectedReportId.value.code);
+const selectedReportId = ref();
+const reports = ref([
+  { name: 'Babergh', code: 'pt' },
+  { name: 'Willis', code: 'zm' },
+  { name: 'Another one', code: 'kioy' }
+]);
 
 // Note these both havwe to be computed. Using refs direct doesn't work.
-const id = computed(() => selectedReportId.value);
+const id = computed(() => selectedReportId.value?.code);
 const enabled = computed(() => !!selectedReportId.value);
 const { isLoading, data } = useGetV2AcelReportId(id, {
   query: {
@@ -28,9 +22,14 @@ const { isLoading, data } = useGetV2AcelReportId(id, {
 <template>
   <div>
     <h1>ACEL dash - just a GET test - {{ selectedReportId }}</h1>
-    <Button label="report one" @click="selectedReportId = 'zm'" />
-    <Button label="report two" @click="selectedReportId = 'kioy'" />
-    <p v-if="isLoading">Loading...</p>
+    <Dropdown
+      v-model="selectedReportId"
+      :options="reports"
+      optionLabel="name"
+      placeholder="Select a Report"
+      class="w-full md:w-14rem"
+    />
+    <p v-if="isLoading && enabled">Loading...</p>
     <pre> {{ JSON.stringify(data, null, 2) }}</pre>
   </div>
 </template>
