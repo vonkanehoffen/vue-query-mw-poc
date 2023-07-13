a16518a77c30cafc7d4dee071ab80d50fc39c121
Kane Clover <kane.clover@gmail.com>
2023-07-13 08:37:20 +0100

    targeted cache invalidation

--- src/views/Acel/Dashboard/InvalidateButton.vue
+++ src/views/Acel/Dashboard/InvalidateButton.vue
@@ -3,7 +3,11 @@ import { useQueryClient } from '@tanstack/vue-query';
 import Button from 'primevue/button';
 
 const queryClient = useQueryClient();
+
+const handleInvalidate = () => {
+  queryClient.invalidateQueries({ queryKey: ['v2', 'acel'] });
+};
 </script>
 <template>
-  <Button label="Invalidate cache" @click="queryClient.invalidateQueries()" />
+  <Button label="Invalidate cache" @click="handleInvalidate()" />
 </template>
